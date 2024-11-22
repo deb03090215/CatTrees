054e5c706f2de27d62137db38eeb23dc8e98707c
Tai-Jung Li <79965615+deb03090215@users.noreply.github.com>
2024-11-22 12:42:45 +0800

    Add files via upload

--- server.R
+++ server.R
@@ -1,165 +1,115 @@
-pacman::p_load(shiny)
-pacman::p_load(shinydashboard)
-pacman::p_load(ggplot2)
-pacman::p_load(ggtree)
-pacman::p_load(showtext)
-pacman::p_load(RColorBrewer)
-pacman::p_load(magrittr)
-pacman::p_load(tidytree)
-pacman::p_load(plotly)
-pacman::p_load(phytools)
-pacman::p_load(ape)
-pacman::p_load(treeio)
-pacman::p_load(phangorn)
-pacman::p_load(grid)
-pacman::p_load(gridExtra)
-pacman::p_load(dplyr)
-pacman::p_load(ggrepel)
-pacman::p_load(scales)
-pacman::p_load(dendextend)
-pacman::p_load(ggnewscale)
-pacman::p_load(shadowtext)
-pacman::p_load(TDbook)
-pacman::p_load(highcharter)
-pacman::p_load(shinyjqui)
-pacman::p_load(tidyverse)
-pacman::p_load(fivethirtyeight)
-pacman::p_load(grDevices)
-pacman::p_load(Cairo)
-pacman::p_load(vegan)
+# Load required packages
+pacman::p_load(shiny, shinydashboard, ggplot2, ggtree, showtext, RColorBrewer, magrittr, tidytree, 
+               plotly, phytools, ape, treeio, phangorn, grid, gridExtra, dplyr, ggrepel, scales, 
+               dendextend, ggnewscale, shadowtext, highcharter, shinyjqui, tidyverse, Cairo)
 
-shinyServer(function(input, output){
+# Define Shiny server logic
+shinyServer(function(input, output) {
+  
+  # Load and process input tree files
   tree <- reactive({
-    inFile <- req(input$file1)
-    if (is.null(inFile)){
-      return(NULL)
-    }else {
-      
-      numfiles = nrow(inFile)
-      tree_list = list()
-      
-      for (i in 1:numfiles){
-        tree_list[[i]] = read.tree(input$file1$datapath[i])
-      }
-      return(tree_list)
+    validate(need(input$file1, "Please upload at least one valid tree file."))
+    inFile <- req(input$file1)  # Require the input file
+    tree_list <- lapply(inFile$datapath, function(path) {
+      tryCatch({
+        read.tree(path)
+      }, error = function(e) {
+        NULL  # Return NULL if a tree file cannot be read
+      })
+    })
+    
+    # Check for any invalid files
+    if (any(sapply(tree_list, is.null))) {
+      showNotification("Some files could not be read. Please check the format.", type = "error")
     }
+    
+    return(tree_list)
   })
-
-    ## reassortment strains file
-    rst <- reactive({
-      req(input$file3)
-      df <- read.table(input$file3$datapath, sep = "\t",header = TRUE)
-      return(df)
-    })
+  
+  # Load reassortment strains file
+  rst <- reactive({
+    req(input$file3)  # Require the input file
+    df <- read.table(input$file3$datapath, sep = "\t",header = TRUE)
+    return(df)
+  })
+  
+  # Load clade information
+  clade <- reactive({
+    req(input$file4)  # Require the input file
+    df <- read.csv(input$file4$datapath,sep = "\t", header = TRUE)
+    return(df)
+  })
+  
+  # Generate the plot with progress bar
+  plotInput <- reactive({
+    validate(need(tree(), "Please upload tree files to generate the plot."))
+    validate(need(clade(), "Clade information is required for plotting."))
     
-    ## clade file
-    clade <- reactive({
-        req(input$file4)
-        df <- read.csv(input$file4$datapath,sep = "\t", header = TRUE)
-        return(df)
-    })
-    
-    ## 
-    plotInput <- reactive({
-        ## make 2 empty list for "for loop"
-        infor <- list()
-        df <- list()
-        
-        ## build reference tree at first
-        ## import reference & clade information
-        x <- tree()[[1]]
-        cl <- clade()
-        
-        #highlight clade
-        #cl_list <- list(Clade = cl[!duplicated(cl$clade),])
-        #cl_list <- factor(cl$clade, levels = cl_list$Clade)
-        
-        
-        p1 <- ggtree(x) %<+% cl + 
-              geom_tippoint(aes(color=Clade), size=input$p, shape=15)
-        
-        infor[[1]] <- p1
-        df[[1]] <- infor[[1]]$data
-
-        ## combine reference tree and other segment trees
-        combined_tree <- infor[[1]]
-
-        sum<-length(tree())
-       
-        ## retrieve max of x and file name
-        label_pos <- max(df[[1]]$x)+input$distance
-        label_name <- input$file1$name[1]
-        
-        for (i in 2:sum) {
-        ## import segment files
-
-            x<-tree()[[i]]
-
-            #highlight clade
-            p2<- ggtree(x) %<+% cl + geom_tippoint(aes(color=Clade), size=input$p, shape=15)
-
-            infor[[i]] <- p2
-            df[[i]] <- infor[[i]]$data
-
-            ## distance between trees. 0.001 and 1 for divergence and time tree, respectively
-            df[[i]]$x <- (df[[i]]$x + max(df[[i-1]]$x) + input$distance) + max(df[[1]]$x) + input$distance
-
-            ## append tree to init tree
-            combined_tree <- combined_tree +
-                geom_tree(data=df[[i]]) +
-                geom_tippoint(data=df[[i]], aes(color=Clade), size=input$p, shape=15) + 
-              labs(input$file1$name[i])
-
-            ## retrieve max of x and file name  
-            label_pos <- append(label_pos, max(df[[i]]$x)+input$distance)
-            label_name <- append(label_name, input$file1$name[i])
-        }
-
-        ## annotate file name based on their x positions
-        #combined_tree <- combined_tree + annotate("text", x = label_pos, y=5, label = label_name)
-        combined_tree <- combined_tree + annotate("text", x = label_pos, y=70, side=1, label = label_name)
-        
-        ## only keep tips for annotating lines
-        combined_df <- bind_rows(df[1:8]) %>% filter(isTip == TRUE)
-        print(combined_df$label)
-        ## plot trees
-        r <- rst()
-    
-        ### split the data for annotating different color and size
-        rassort <- subset(combined_df, label %in% r$strain)
-        #print(rassort)
-
-        non_rassort <- subset(combined_df, !(label %in% r$strain))
+    withProgress(message = "Generating Tree...", {
+      infor <- list()
+      df <- list()
       
-        #print(non_rassort)
-
-        combined_tree_w_rassort <- combined_tree + geom_line(data = rassort,
-                                  aes(x, y, group = label),
-                                  size = input$z,
-                                  color = "#ff0000")
-    
-        combined_tree_w_rassort + geom_line(data = non_rassort,
-                                     aes(x, y, group = label),
-                                    size = input$n,
-                                   color = "#646464")
-        })
-    
-      #output showed in the interface
-      output$treeplot_normal <- renderPlot({
-        print(plotInput())
-      })
+      # Initialize the combined tree
+      x <- tree()[[1]]
+      cl <- clade()
+      combined_tree <- ggtree(x) %<+% cl + 
+        geom_tippoint(aes(color = Clade), size = input$p, shape = 15)
       
-      #Download file
-      output$downloadPlot <- downloadHandler(
-      filename = "report.png" ,
-      tags$style(".skin-blue .sidebar a { color: #000000 }"),
-      content = function(file) {
-        png(file,
-            height = input$y,
-            width = input$x, 
-            res = input$r)
-        print(plotInput())
-        dev.off()
-      })
-
+      infor[[1]] <- combined_tree
+      df[[1]] <- infor[[1]]$data
+      label_pos <- max(df[[1]]$x) + input$distance
+      label_name <- input$file1$name[1]
+      
+      # Append additional trees
+      for (i in 2:length(tree())) {
+        x <- tree()[[i]]
+        p <- ggtree(x) %<+% cl + 
+          geom_tippoint(aes(color = Clade), size = input$p, shape = 15)
+        
+        df[[i]] <- p$data
+        df[[i]]$x <- df[[i]]$x + max(df[[i - 1]]$x) + input$distance
+        
+        combined_tree <- combined_tree +
+          geom_tree(data = df[[i]]) +
+          geom_tippoint(data = df[[i]], aes(color = Clade), size = input$p, shape = 15)
+        
+        label_pos <- append(label_pos, max(df[[i]]$x) + input$distance)
+        label_name <- append(label_name, input$file1$name[i])
+        
+        # Update progress
+        incProgress(1 / length(tree()), detail = paste("Processing tree", i, "of", length(tree())))
+      }
+      
+      # Annotate tree with file names
+      combined_tree <- combined_tree + 
+        annotate("text", x = label_pos, y = 70, label = label_name)
+      
+      # Highlight reassortment strains
+      combined_df <- bind_rows(df) %>% filter(isTip == TRUE)
+      r <- rst()
+      reassort <- subset(combined_df, label %in% r$strain)
+      non_reassort <- subset(combined_df, !(label %in% r$strain))
+      
+      combined_tree <- combined_tree +
+        geom_line(data = reassort, aes(x, y, group = label), size = input$z, color = "#ff0000") +
+        geom_line(data = non_reassort, aes(x, y, group = label), size = input$n, color = "#646464")
+      
+      return(combined_tree)
+    })
+  })
+  
+  # Render the tree plot in the Shiny app
+  output$treeplot_normal <- renderPlot({
+    print(plotInput())
+  })
+  
+  # Allow users to download the plot
+  output$downloadPlot <- downloadHandler(
+    filename = "tree_plot.png",
+    content = function(file) {
+      png(file, height = input$y, width = input$x, res = input$r)
+      print(plotInput())
+      dev.off()
+    }
+  )
 })
--- ui.R
+++ ui.R
@@ -1,122 +1,120 @@
-pacman::p_load(shiny)
-pacman::p_load(shinydashboard)
-pacman::p_load(plotly)
-pacman::p_load(highcharter)
-pacman::p_load(shinyjqui)
-pacman::p_load(grDevices)
-pacman::p_load(shinyBS)
+# Load required packages
+pacman::p_load(shiny, shinydashboard, plotly, highcharter, shinyjqui, grDevices, shinyBS)
 
+# Define the dashboard sidebar
 sidebar <- dashboardSidebar(
-sidebarMenu(
+  sidebarMenu(
+    # Dashboard menu item
     menuItem("Dashboard", 
              tabName = "tab_normal",
              icon = icon("dashboard")),
+    
+    # File input for phylogenetic trees with help popover
     div(
       div(
-        style="width:80%; display:inline-block; vertical-align: middle;",
+        style = "width:80%; display:inline-block; vertical-align: middle;",
         fileInput('file1', 'Phylogenetic trees',
-                  accept=c('text/csv', 'text/comma-separated-values,text/plain',
-                           '.nwk', '.phy', '.jplace', '.jtree', '.tree', '.tre'),
-                  multiple = T),
+                  accept = c('text/csv', 'text/plain', '.nwk', '.phy', '.jplace', '.jtree', '.tree', '.tre'),
+                  multiple = TRUE)
       ),
       div(
-        style="display:inline-block; vertical-align: middle;",
-        bsButton("q1", label = "", icon = icon("info"),
-                 style = "info", size = "extra-small"),
+        style = "display:inline-block; vertical-align: middle;",
+        bsButton("q1", label = "", icon = icon("info"), style = "info", size = "extra-small"),
         bsPopover(id = "q1", title = "Help",
-                  content = paste0("Please upload two or more tree files with format `.nwk`.",
+                  content = paste0("Please upload two or more tree files in `.nwk` format.", 
                                    br(),
-                                   "Recommended sequence number: below 1000"),
-                  placement = "right", 
-                  trigger = "click", 
-                  options = list(container = "body")
-        )
+                                   "Recommended: fewer than 1000 sequences per tree."),
+                  placement = "right",
+                  trigger = "click",
+                  options = list(container = "body"))
       )
     ),
+    
+    # File input for reassortment strains with help popover
     div(
       div(
-        style="width:80%; display:inline-block; vertical-align: middle;",
+        style = "width:80%; display:inline-block; vertical-align: middle;",
         fileInput('file3', 'Reassortment strains',
-                  accept=c('text/csv', 'text/comma-separated-values,text/plain',
-                           '.csv')),
+                  accept = c('text/csv', 'text/plain', '.csv'))
       ),
       div(
-        style="display:inline-block; vertical-align: middle;",
-        bsButton("q2", label = "", icon = icon("info"),
-                 style = "info", size = "extra-small"),
+        style = "display:inline-block; vertical-align: middle;",
+        bsButton("q2", label = "", icon = icon("info"), style = "info", size = "extra-small"),
         bsPopover(id = "q2", title = "Help",
-                  content = paste0("Please upload a csv file containg a list of reassortment strains.",
+                  content = paste0("Please upload a CSV file containing a list of reassortment strains.", 
                                    br(),
-                                   "Titile of the list must be `strain`."),
-                  placement = "right", 
-                  trigger = "click", 
-                  options = list(container = "body")
-        )
+                                   "The column title must be `strain`."),
+                  placement = "right",
+                  trigger = "click",
+                  options = list(container = "body"))
       )
     ),
+    
+    # File input for clade information with help popover
     div(
       div(
-        style="width:80%; display:inline-block; vertical-align: middle;",
+        style = "width:80%; display:inline-block; vertical-align: middle;",
         fileInput('file4', 'Clade',
-                  accept=c('text/csv', 'text/comma-separated-values,text/plain',
-                           '.csv')),
-        ),
+                  accept = c('text/csv', 'text/plain', '.csv'))
+      ),
       div(
-        style="display:inline-block; vertical-align: middle;",
-        bsButton("q3", label = "", icon = icon("info"),
-                 style = "info", size = "extra-small"),
+        style = "display:inline-block; vertical-align: middle;",
+        bsButton("q3", label = "", icon = icon("info"), style = "info", size = "extra-small"),
         bsPopover(id = "q3", title = "Help",
-                  content = paste0("Please upload a csv file containing strains and their corresponding clades.", 
+                  content = paste0("Please upload a CSV file with clade information.", 
                                    br(),
-                                   "Titile of two columns must be `Taxa` and `Clade`."),
-                  placement = "right", 
+                                   "Columns must be titled `Taxa` and `Clade`."),
+                  placement = "right",
                   trigger = "click",
-                  options = list(container = "body")
-        )
+                  options = list(container = "body"))
       )
     ),
+    
+    # Settings menu for tree display options
     menuItem("Settings", 
              icon = icon("tools"),
              sliderInput("distance", "Distance between trees",
-                         min = 0.000001, max = 1, value = c(0.000001)
-                         ),
+                         min = 0.000001, max = 1, value = 0.000001),
              sliderInput("z", "Size of reassortant line",
-                         min = 0.05, max = 1,value = c(0.3)
-                         ),
+                         min = 0.05, max = 1, value = 0.3),
              sliderInput("n", "Size of gray line",
-                         min = 0.005, max = 1,
-                         value = c(0.005)
-                         ),
+                         min = 0.005, max = 1, value = 0.005),
              sliderInput("p", "Point size",
-                         min = 0.1, max = 2,
-                         value = c(0.8)
-                         )
-             ),
+                         min = 0.1, max = 2, value = 0.8)
+    ),
+    
+    # Picture size settings
     menuItem("Picture size",
              icon = icon("photo"),
              numericInput("x", "Width", 3000),
-             numericInput("y", "Hight", 1000),
-             numericInput("r", "Resolution(dpi)", 300)
+             numericInput("y", "Height", 1000),
+             numericInput("r", "Resolution (dpi)", 300)
     )
-))
+  )
+)
 
+# Define the dashboard tab content
 tab_normal <- fluidRow(
-  box(plotOutput("treeplot_normal"), width = "100%", height = "100%",dpi=3000)
+  box(plotOutput("treeplot_normal", height = "600px"), width = 12)
 )
 
+# Define the dashboard body
 body <- dashboardBody(
-    tabItems(
-        tabItem(tabName  = "tab_normal", tab_normal)
-    ),
-    downloadButton("downloadPlot", "Download", class = "butt3")
+  tabItems(
+    tabItem(tabName = "tab_normal", tab_normal)  # Main tab for tree plots
+  ),
+  downloadButton("downloadPlot", "Download", class = "butt3")  # Download button
 )
 
-header <- dashboardHeader(title = h4(HTML("CatTrees"),
-                          style = "font-size: 32px")
-                          )
+# Define the dashboard header
+header <- dashboardHeader(
+  title = h4(HTML("CatTrees"), style = "font-size: 32px")
+)
 
-dashboardPage(skin = "blue",
-              dashboardHeader(title = header),
-              sidebar,
-              body
-              )
+# Assemble the dashboard page
+dashboardPage(
+  skin = "blue",
+  header,
+  sidebar,
+  body
+)
